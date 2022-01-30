d6409d62a7871f4fe60964b37b7b607408577068
Md. Alif Biswas <md.alifbiswas@gmail.com>
2022-01-30 21:04:13 +0600

    Design Secret Sync API (#3)
    Added controller

--- controllers/secretsync_controller.go
+++ controllers/secretsync_controller.go
@@ -18,6 +18,10 @@ package controllers
 
 import (
 	"context"
+	core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -36,6 +40,8 @@ type SecretSyncReconciler struct {
 //+kubebuilder:rbac:groups=sync.spectro30,resources=secretsyncs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=sync.spectro30,resources=secretsyncs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=sync.spectro30,resources=secretsyncs/finalizers,verbs=update
+//+kubebuilder:rbac:groups=v1,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=v1,resources=secrets/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,9 +53,54 @@ type SecretSyncReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *SecretSyncReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
+	var secretSync syncv1.SecretSync
+	if err := r.Get(ctx, req.NamespacedName, &secretSync); err != nil {
+		log.Error(err, "unable to fetch SecretSync")
+		// we'll ignore not-found errors, since they can't be fixed by an immediate
+		// requeue (we'll need to wait for a new notification), and we can get them
+		// on deleted requests.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	// TODO(user): your logic here
+	if secretSync.Spec.Paused != nil && *secretSync.Spec.Paused {
+		log.V(1).Info("secretSync suspended, skipping")
+		return ctrl.Result{}, nil
+	}
+
+	var sourceSecret core.Secret
+	if err := r.Get(ctx, types.NamespacedName{
+		Namespace: secretSync.Spec.SourceRef.Namespace,
+		Name:      secretSync.Spec.SourceRef.Name,
+	}, &sourceSecret); err != nil {
+		log.Error(err, "unable to fetch Secret")
+		return ctrl.Result{RequeueAfter: time.Minute}, err
+	}
+
+	createNameSpacedSecret := func(sourceSecret *core.Secret, namespace syncv1.NamespaceRef) (*core.Secret, error) {
+		newSecret := &core.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      sourceSecret.Name,
+				Namespace: string(namespace),
+			},
+			Data: sourceSecret.Data,
+		}
+		return newSecret, nil
+	}
+
+	for _, namespace := range secretSync.Spec.TargetNamespaces {
+		newSecret, err := createNameSpacedSecret(&sourceSecret, namespace)
+		log.Info("the namespace is", "namespace", namespace)
+		if err != nil {
+			log.Error(err, "unable to construct secret from template")
+			// don't bother requeuing until we get a change to the spec
+			return ctrl.Result{RequeueAfter: time.Minute}, nil
+		}
+		if err := r.Create(ctx, newSecret); err != nil {
+			log.Error(err, "unable to create secret for SecretSync")
+		}
+		log.V(1).Info("created secret for", "source secret", newSecret.Name)
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -58,5 +109,6 @@ func (r *SecretSyncReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 func (r *SecretSyncReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&syncv1.SecretSync{}).
+		Owns(&core.Secret{}).
 		Complete(r)
 }
